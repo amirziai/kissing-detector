cda3d270f2ea154d98a51fcfd7531683543670fa
Amir Ziai <arziai@gmail.com>
2019-06-03 15:30:35 -0700

    added conv3d

--- kissing_detector.py
+++ kissing_detector.py
@@ -1,8 +1,11 @@
+from typing import Optional
+
 import torch
 from torch import nn
+
 import vggish
 from conv import convnet_init
-from typing import Optional
+import conv3d
 
 
 class KissingDetector(nn.Module):
@@ -24,6 +27,48 @@ class KissingDetector(nn.Module):
                                                                    num_classes,
                                                                    feature_extract,
                                                                    use_pretrained)
+        if use_vggish:
+            vggish_model, vggish_output_size = vggish.vggish(feature_extract)
+
+        if not conv and not vggish_model:
+            raise ValueError("Use VGGish, Conv, or both")
+
+        self.conv_input_size = conv_input_size
+        self.conv = conv
+        self.vggish = vggish_model
+        self.combined = nn.Linear(vggish_output_size + conv_output_size, num_classes)
+
+    def forward(self, audio: torch.Tensor, image: torch.Tensor):
+        a = self.vggish(audio) if self.vggish else None
+        c = self.conv(image) if self.conv else None
+
+        if a is not None and c is not None:
+            combined = torch.cat((c.view(c.size(0), -1), a.view(a.size(0), -1)), dim=1)
+        else:
+            combined = a if a is not None else c
+
+        return self.combined(combined)
+
+
+class KissingDetector3DConv(nn.Module):
+    def __init__(self,
+                 num_classes: int,
+                 feature_extract: bool,
+                 use_vggish: bool = True):
+        super(KissingDetector3DConv, self).__init__()
+        conv_output_size = 512
+        vggish_output_size = 0
+        conv_input_size = 0
+        vggish_model = None
+
+        conv = conv3d.resnet34(
+            num_classes=num_classes,
+            shortcut_type='B',
+            sample_size=224,
+            sample_duration=10
+        )
+        conv.fc = nn.Identity()
+
         if use_vggish:
             vggish_model, vggish_output_size = vggish.vggish(feature_extract)
 
--- qualitative.py
+++ qualitative.py
@@ -94,3 +94,101 @@ class QualitativeAnalysis:
             y = [1 if 'kissing' in vid else 0] * len(A)
             self._show_saliency_maps(A, I, y)
             print('=' * 10)
+
+    # next few methods taken from cs231n
+    @staticmethod
+    def jitter(X, ox, oy):
+        """
+        Helper function to randomly jitter an image.
+
+        Inputs
+        - X: PyTorch Tensor of shape (N, C, H, W)
+        - ox, oy: Integers giving number of pixels to jitter along W and H axes
+
+        Returns: A new PyTorch Tensor of shape (N, C, H, W)
+        """
+        if ox != 0:
+            left = X[:, :, :, :-ox]
+            right = X[:, :, :, -ox:]
+            X = torch.cat([right, left], dim=3)
+        if oy != 0:
+            top = X[:, :, :-oy]
+            bottom = X[:, :, -oy:]
+            X = torch.cat([bottom, top], dim=2)
+        return X
+
+    def create_class_visualization(target_y, model, dtype, **kwargs):
+        """
+        Generate an image to maximize the score of target_y under a pretrained model.
+
+        Inputs:
+        - target_y: Integer in the range [0, 1000) giving the index of the class
+        - model: A pretrained CNN that will be used to generate the image
+        - dtype: Torch datatype to use for computations
+
+        Keyword arguments:
+        - l2_reg: Strength of L2 regularization on the image
+        - learning_rate: How big of a step to take
+        - num_iterations: How many iterations to use
+        - blur_every: How often to blur the image as an implicit regularizer
+        - max_jitter: How much to gjitter the image as an implicit regularizer
+        - show_every: How often to show the intermediate result
+        """
+        model.type(dtype)
+        l2_reg = kwargs.pop('l2_reg', 1e-3)
+        learning_rate = kwargs.pop('learning_rate', 25)
+        num_iterations = kwargs.pop('num_iterations', 100)
+        blur_every = kwargs.pop('blur_every', 10)
+        max_jitter = kwargs.pop('max_jitter', 16)
+        show_every = kwargs.pop('show_every', 25)
+
+        # Randomly initialize the image as a PyTorch Tensor, and make it requires gradient.
+        img = torch.randn(1, 3, 224, 224).mul_(1.0).type(dtype).requires_grad_()
+
+        for t in range(num_iterations):
+            # Randomly jitter the image a bit; this gives slightly nicer results
+            ox, oy = random.randint(0, max_jitter), random.randint(0, max_jitter)
+            img.data.copy_(jitter(img.data, ox, oy))
+
+            ########################################################################
+            # TODO: Use the model to compute the gradient of the score for the     #
+            # class target_y with respect to the pixels of the image, and make a   #
+            # gradient step on the image using the learning rate. Don't forget the #
+            # L2 regularization term!                                              #
+            # Be very careful about the signs of elements in your code.            #
+            ########################################################################
+            # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+            target = model(img)[0, target_y]
+            target.backward()
+            g = img.grad.data
+            g -= 2 * l2_reg * img.data
+            img.data += learning_rate * (g / g.norm())
+            img.grad.zero_()
+
+            # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+            ########################################################################
+            #                             END OF YOUR CODE                         #
+            ########################################################################
+
+            # Undo the random jitter
+            img.data.copy_(jitter(img.data, -ox, -oy))
+
+            # As regularizer, clamp and periodically blur the image
+            for c in range(3):
+                lo = float(-SQUEEZENET_MEAN[c] / SQUEEZENET_STD[c])
+                hi = float((1.0 - SQUEEZENET_MEAN[c]) / SQUEEZENET_STD[c])
+                img.data[:, c].clamp_(min=lo, max=hi)
+            if t % blur_every == 0:
+                blur_image(img.data, sigma=0.5)
+
+            # Periodically show the image
+            if t == 0 or (t + 1) % show_every == 0 or t == num_iterations - 1:
+                plt.imshow(deprocess(img.data.clone().cpu()))
+                class_name = class_names[target_y]
+                plt.title('%s\nIteration %d / %d' % (class_name, t + 1, num_iterations))
+                plt.gcf().set_size_inches(4, 4)
+                plt.axis('off')
+                plt.show()
+
+        return deprocess(img.data.cpu())
